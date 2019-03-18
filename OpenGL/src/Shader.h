d6abcee7ffa15b844664cedbca9de359f89c93a6
Unknown <franizus@live.com>
2019-03-18 13:32:13 -0500

    shader abstraction

---
+++ OpenGL/src/Shader.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+class Shader
+{
+private:
+	unsigned int m_RendererID;
+	std::string m_FilePath;
+	std::unordered_map<std::string, int> m_UniformLocationCache;
+
+public:
+	Shader(const std::string& filepath);
+	~Shader();
+
+	void bind() const;
+	void unbind() const;
+
+	// Set uniforms
+	void setUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+	void setUniform1f(const std::string& name, float value);
+
+private:
+	int getUniformLocation(const std::string& name);
+	struct ShaderProgramSource parseShader(const std::string& filepath);
+	unsigned int compileShader(unsigned int type, const std::string& source);
+	unsigned int createShader(const std::string& vertexShader, const std::string& fragmentShader);
+
+};
